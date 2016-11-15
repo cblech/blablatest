a6001539d787ffe2f11f142633817ad560ec585a
luebeck.jonathan <luebeck.jonathan@B201PC715.berufskolleg-aachen.de>
2016-11-15 10:10:26 +0100

    asdfa

---
+++ kljhasgldbfasfdga/src/kljhasgldbfasfdga/xcgsdfh.java
@@ -0,0 +1,23 @@
+package kljhasgldbfasfdga;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+public class xcgsdfh extends JFrame{
+	public xcgsdfh() {
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.WEST);
+		
+		JPanel panel_1 = new JPanel();
+		getContentPane().add(panel_1, BorderLayout.EAST);
+		
+		JPanel panel_2 = new JPanel();
+		getContentPane().add(panel_2, BorderLayout.NORTH);
+		
+		JPanel panel_3 = new JPanel();
+		getContentPane().add(panel_3, BorderLayout.SOUTH);
+	}
+
+}
